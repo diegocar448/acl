e553470074a604f29c62bdbbaaec485d81055b14
diegocar448 <diegocar448@hotmail.com>
2018-08-06 10:53:03 -0300

    gerar migration de posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Post $post)
     {
-        return view('home');
+        $posts = $post->all();
+
+
+        return view('home', compact('posts'));
     }
 }
