ac42d4775c232ba9e9e3179636d0dc764b4a34e6
diegocar448 <diegocar448@hotmail.com>
2018-08-18 15:26:23 -0300

    project continue

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
         /*
         Post::class  => PostPolicy::class,
        */
-
+  
         
     ];
 
--- app/User.php
+++ app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Permission;
+use Roles;
 
 class User extends Authenticatable
 {
@@ -19,11 +21,35 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //Metodo que retorna todas as funções que o usuario possui
+    public function roles()
+    {
+        //Pegar todos os papeis que o cliente tem no sistema
+        $this->belongsToMany(Roles::class);
+    }
+
+
+
+    public function hasPermission(Permission $permission)
+    {
+        //Vamos recuperar todas as nossas funções que temos para essa permissão
+         //view_post -> Manager, Admin
+         //o usuario logado ele têm acesso, se têm retorna true, se não bloqueia acesso
+         return $this->hasAnyRoles($permission->roles);
+    }
+
+    public function hasAnyRoles($roles)
+    {
+        //aqui vamos verificar se o usuario logado têm essa permissão especifica
+
+
+    }
 }
